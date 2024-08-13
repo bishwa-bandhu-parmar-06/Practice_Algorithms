65cc75214f026fccf03f6e668990eeabfa082130
BISHWA BANDHU PARMAR <bishwabandhuparmar06@gmail.com>
2024-08-13 23:31:53 +0530

    Add files via upload

---
+++ inheritance.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+class a {
+    int a;
+    int b;
+    public : 
+        void setvalue(){
+            int a = 5;
+            int b =  6;
+        }
+};
+class b:public a{
+    public: void sum(){
+        
+    }
+}
+int main(){
+
+}
